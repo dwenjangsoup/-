7b6a86d91b0ed5de67fb93ebf7090a693ef299dc
dwenjangsoup <103800274+dwenjangsoup@users.noreply.github.com>
2022-04-16 11:57:42 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Mbtipage from "./Mbtipage";
+import Loadingcaterpillar from "./Loadingcaterpillar";
+function App() {
+  return (
+    <div>
+      <Loadingcaterpillar />
+    </div>
+  );
+}
+
+export default App;
